7b8ddf2b691fa48ce56ecf3bc82c3fcb0cdcee6d
dm937 <dm937@cam.ac.uk>
2022-02-09 10:36:24 +0000

    added geo test file

--- test_geo.py
+++ test_geo.py
@@ -1,1 +1,9 @@
-# empty right now
+import floodsystem.geo
+from floodsystem.stationdata import build_station_list
+
+
+def test_call_stations_by_distance():
+    x = floodsystem.geo.stations_by_distance(build_station_list(), (52.2053, 0.1218))
+
+def test_correct_lengths():
+     
