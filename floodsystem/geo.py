41137667ea0dfda5f796bd7795bb16dbcfe1f341
dm937 <dm937@cam.ac.uk>
2022-02-09 10:45:27 +0000

    finished test for task 1b and altered 1b to produce floats for distance not int

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -14,7 +14,7 @@ def stations_by_distance(stations, p):
     StationsDistance = [""]*len(stations)
     i = 0
     for station in stations:
-        StationsDistance[i] = (station.name, station.town) + (int(haversine(station.coord, p)),)
+        StationsDistance[i] = (station.name, station.town) + (haversine(station.coord, p),)
         i += 1
     return sorted_by_key(StationsDistance, 2)
 
