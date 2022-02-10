de7073ad52186ae0ba74474cdf8c330ecd4839e9
dm937 <dm937@cam.ac.uk>
2022-02-10 14:56:49 +0000

    1st attempt to solve github pytest

--- test_geo.py
+++ test_geo.py
@@ -1,34 +1,34 @@
 from distutils.command import build
-import floodsystem.geo
+from floodsystem.geo import stations_by_distance, stations_within_radius, rivers_by_station_number, rivers_with_station, stations_by_river
 from floodsystem.stationdata import build_station_list
 AllStations = build_station_list()
 
 def test_call_stations_by_distance():
-    x = floodsystem.geo.stations_by_distance(AllStations, (52.2053, 0.1218))
+    x = stations_by_distance(AllStations, (52.2053, 0.1218))
 
 def test_correct_lengths_stations_by_distance():
-     assert len(build_station_list()) == len(floodsystem.geo.stations_by_distance(AllStations, (52.2053, 0.1218)))
+     assert len(build_station_list()) == len(stations_by_distance(AllStations, (52.2053, 0.1218)))
      # checking special values don't affect the program
-     assert len(AllStations) == len(floodsystem.geo.stations_by_distance(AllStations, (0, 0)))
-     assert len(AllStations) == len(floodsystem.geo.stations_by_distance(AllStations, (-90, 90)))
-     assert len(AllStations) == len(floodsystem.geo.stations_by_distance(AllStations, (90, -90)))
+     assert len(AllStations) == len(stations_by_distance(AllStations, (0, 0)))
+     assert len(AllStations) == len(stations_by_distance(AllStations, (-90, 90)))
+     assert len(AllStations) == len(stations_by_distance(AllStations, (90, -90)))
 
 def test_stations_within_radius():
-    assert len(floodsystem.geo.stations_within_radius(AllStations, (52.2053, 0.1218), 10000)) == len(AllStations)
-    assert len(floodsystem.geo.stations_within_radius(AllStations, (52.2053, 0.1218), 10)) > 0
-    assert len(floodsystem.geo.stations_within_radius(AllStations, (52.2053, 0.1218), 1)) < len(floodsystem.geo.stations_within_radius(AllStations, (52.2053, 0.1218), 10))
+    assert len(stations_within_radius(AllStations, (52.2053, 0.1218), 10000)) == len(AllStations)
+    assert len(stations_within_radius(AllStations, (52.2053, 0.1218), 10)) > 0
+    assert len(stations_within_radius(AllStations, (52.2053, 0.1218), 1)) < len(stations_within_radius(AllStations, (52.2053, 0.1218), 10))
 
-def rivers_by_station_number(stations, N):
-    river_test = floodsystem.geo.rivers_by_station_number(AllStations, 10)
+def test_rivers_by_station_number():
+    river_test = rivers_by_station_number(AllStations, 10)
     assert len(river_test) >= 1
-    assert river_test[1][1] >= river_test[1][4]
+    assert river_test[1][1] >= river_test[4][1]
     assert type(river_test) == list
     assert type(river_test[2]) == tuple
 
 def test_rivers_with_station():
-    assert len(floodsystem.geo.rivers_with_station(AllStations)) > 0
-    assert len(floodsystem.geo.rivers_with_station([])) == 0
+    assert len(rivers_with_station(AllStations)) > 0
+    assert len(rivers_with_station([])) == 0
 
 def test_stations_with_river():
-    assert len(floodsystem.geo.stations_by_river(AllStations)) > 0
-    assert len(floodsystem.geo.stations_by_river([])) == 0
+    assert len(stations_by_river(AllStations)) > 0
+    assert len(stations_by_river([])) == 0
