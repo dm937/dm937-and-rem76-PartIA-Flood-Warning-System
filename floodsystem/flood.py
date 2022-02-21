4cfeeed33952860675604af0ffda77e8fc797e6a
rob-rem76 <rem76@cam.ac.uk>
2022-02-21 14:05:23 +0000

    Progress on task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,2 +1,8 @@
+
+import numpy as py
+
+
+
 def stations_highest_rel_level(stations, N):
-    pass
+    range_ratios = [i, i.typical_range for i in stations]
+    sorted_range_ratios = range_ratios.sorted()
