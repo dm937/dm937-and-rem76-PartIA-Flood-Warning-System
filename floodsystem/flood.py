c0503754364b6d74e0b1bb823921dfaffa9158f8
dm937 <dm937@cam.ac.uk>
2022-02-26 15:15:17 +0000

    almost completed task2b still need to add pytest however

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,8 +1,28 @@
 
 import numpy as py
 
+from floodsystem.station import MonitoringStation
+
 
 
 def stations_highest_rel_level(stations, N):
     range_ratios = [i, i.typical_range for i in stations]
     sorted_range_ratios = range_ratios.sorted()
+
+
+def stations_level_over_threshold(stations, tol):
+    '''
+    returns a list of tuples, where each tuple holds (i) a station (object) 
+    at which the latest relative water level is over tol 
+    and (ii) the relative water level at the station. 
+    '''
+    Stations_over_tol = []
+    # loops through the stations
+    for station in stations:
+        # checks if relative water level is over that of the tolerance
+        if MonitoringStation.relative_water_level(station) > tol:
+            # adds the staion and relative water level tuple to the list
+            Stations_over_tol.append((station, MonitoringStation.relative_water_level(station)))
+    # sorts the list in descending order by the relative water level
+    sorted(Stations_over_tol, key=lambda tup: tup[1], reverse=True)
+    return Stations_over_tol
--- floodsystem/station.py
+++ floodsystem/station.py
@@ -61,7 +61,11 @@ class MonitoringStation:
         '''
         returns the current water level as a fraction of its average
         '''
-        
+        # makes sure the range is consistent 
+        if MonitoringStation.typical_range_consistent(self) == False or self.latest_level == None:
+            return None
+        # if range is consistent and a latest level exists it will give the current range as a fraction of the typical range 
+        return  ((self.latest_level - self.typical_range[1])/(self.typical_range[0] - self.typical_range[1]))
 
 
 def inconsistent_typical_range_stations(stations):
