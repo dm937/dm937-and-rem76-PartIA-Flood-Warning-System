62ccf01e24b70321b6d4b05566830b8b90d543d7
rob-rem76 <rem76@cam.ac.uk>
2022-02-26 23:51:32 +0000

    Minor changes to flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,8 +1,8 @@
 
 import numpy as py
-
+from floodsystem.station import MonitoringStation
 
 
 def stations_highest_rel_level(stations, N):
-    range_ratios = [i, i.typical_range for i in stations]
+    range_ratios = [(i, i.typical_range) for i in stations]
     sorted_range_ratios = range_ratios.sorted()
