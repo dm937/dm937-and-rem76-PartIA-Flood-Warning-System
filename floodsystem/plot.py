4510fe8c2aaf5f45b9c9a76127bab544da0d4bb5
rob-rem76 <rem76@cam.ac.uk>
2022-02-27 18:28:07 +0000

    Minor changes

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import numpy as np
 
-def plot_water_levels():
-    t = [datetime(2016, 12, 30), datetime(2016, 12, 31), datetime(2017, 1, 1),
-        datetime(2017, 1, 2), datetime(2017, 1, 3), datetime(2017, 1, 4),
-        datetime(2017, 1, 5)]
-    level = [0.2, 0.7, 0.95, 0.92, 1.02, 0.91, 0.64]
+def plot_water_levels(station, dates, levels):
+    
+    
 
     # Plot
     plt.plot(t, level)
