295021c2e36ebb39b1103b74133cadb27a215872
dm937 <dm937@cam.ac.uk>
2022-02-27 16:30:02 +0000

    changes

--- Task2C.py
+++ Task2C.py
@@ -12,5 +12,5 @@ def run():
 
 
 if __name__ == "__main__":
-    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
     run()
