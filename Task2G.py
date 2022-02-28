14f62c85b88d1100610a70f6a2aaf88f5c10027c
dm937 <dm937@cam.ac.uk>
2022-02-28 19:18:17 +0000

    merging

--- Task2G.py
+++ Task2G.py
@@ -4,8 +4,18 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     # aiming to return the number of sattions that are at severe, high, moderate and low risk
-    stations = 'hi'
-    return '{} stations are at severe risk of flooding, {} stations are at high risk of flooding, {} stations are at moderate risk of flooding and {} stations are at low risk of flooding'
+    stations = build_station_list()
+    Nsevere, Nhigh, Nmoderate, Nlow = 0,0,0,0
+    for station in stations:
+        if flood_risk_assessment(station) =='severe':
+            Nsevere +=1
+        if flood_risk_assessment(station) =='high':
+            Nhigh +=1
+        if flood_risk_assessment(station) =='moderate':
+            Nmoderate += 1
+        else:
+            Nlow +=1
+    return '{} stations are at severe risk of flooding, {} stations are at high risk of flooding, {} stations are at moderate risk of flooding and {} stations are at low risk of flooding'.format(Nsevere, Nhigh, Nmoderate, Nlow)
 
 
 if __name__ == "__main__":
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,6 +1,9 @@
+from logging import raiseExceptions
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.station import MonitoringStation
 import numpy as np
+from datetime import timedelta
+
 
 def flood_risk_assessment(station):
     '''
@@ -10,13 +13,15 @@ def flood_risk_assessment(station):
     moderate - average level over the past 3 days is at a relative level of 1>relative level>0.8
     low - average level over the past 3 days is at a relative level of <0.8
     '''
+    if not station.typical_range_consistent:
+        return None
     days_back = 3
-    dates, levels = fetch_measure_levels(station.measure_id, days_back)
+    dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=days_back))
     if dates == [] or levels ==[]:
         return '{} station has empty levels or dates list'.format(station.name)
     level_array = np.array(levels)
     average_level = np.dot(level_array, np.ones(len(level_array)))
-    relative_level_ot = average_level/station.typical_range[0]
+    relative_level_ot = average_level/station.typical_range[1]
     if relative_level_ot >= 3:
         return 'severe'
     elif 3 > relative_level_ot >=1:
